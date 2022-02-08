be674ae48e34f0777c28e27d2ecc1f65a189ff84
Barmendu Chakraborty <87941013+BarmenduC@users.noreply.github.com>
2022-02-08 17:12:24 +0530

    Create prime_in_range.cpp
    You don't need to read input or print anything. Your task is to complete the function primeRange() which takes 2 integer inputs M and N and returns a list of all primes between M and N.

---
+++ prime_in_range.cpp
@@ -0,0 +1,44 @@
+// { Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    vector<int> primeRange(int M, int N) {
+        // code here
+        vector<int> v;
+        for(int i = M;i <= N ;i++){
+            if(i==0||i==1) continue;
+            bool flag = 0;
+            for(int j = 2; j*j <= i;j++){
+                if(i%j == 0){
+                    flag = 1;
+                    break;
+                }
+            }
+            if(flag == 0) v.push_back(i);
+            
+        }
+        return v;
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N, M, K;
+        cin >> M >> N;
+        Solution ob;
+        vector<int> ans = ob.primeRange(M, N);
+        for (auto u : ans) cout << u << " ";
+        cout << "\n";
+    }
+}  // } Driver Code Ends
