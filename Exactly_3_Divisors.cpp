6cf3123f72bf2c8f70f06b4c9fbd8b5702140ba5
Barmendu Chakraborty <87941013+BarmenduC@users.noreply.github.com>
2022-02-08 12:31:52 +0530

    Create Exactly_3_Divisors.cpp
    You don't need to read input or print anything. Your task is to complete the function exactly3Divisors() that takes N as input parameter and returns count of numbers less than or equal to N with exactly 3 divisors.

---
+++ Exactly_3_Divisors.cpp
@@ -0,0 +1,63 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    int exactly3Divisors(int n)
+   {
+    int count=0;
+       for(int i=2;i*i<=n;i++)
+       {
+           if(IsPrime(i))
+           {
+              count++; 
+           }
+       }
+       
+       return count;
+   }
+   bool IsPrime(int n)
+   {
+       if(n==1) return false;
+       
+       if(n==2 || n==3) return true;
+       
+       if(n%2==0 || n%3==0) return false;
+       
+       for(int i=5;i*i<=n;i=i+6)
+       {
+           if(n%i==0 || n%(i+2)==0) 
+            return false;
+       }
+       return true;
+   }
+};
+
+// { Driver Code Starts.
+
+
+int main()
+ {
+    int T;
+    
+    //taking testcases
+    cin>>T;
+    while(T--)
+    {
+        int N;
+        
+        //taking N
+        cin>>N;
+        Solution ob;
+        //calling function exactly3Divisors()
+        cout<<ob.exactly3Divisors(N)<<endl;
+    }
+	return 0;
+}  // } Driver Code Ends
